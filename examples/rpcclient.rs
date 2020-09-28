80818de0dfa4c379408acdeafc30f5785678750e
Michael Uti <utimichael9@gmail.com>
2020-09-28 17:08:32 +0100

    example/rpcclient: make logging optional

--- examples/rpcclient.rs
+++ examples/rpcclient.rs
@@ -4,36 +4,8 @@ use dcrdrs::{
 };
 use std::{fs, path::PathBuf};
 
-use slog;
-use slog_scope;
-use slog_stdlog;
-use slog_term;
-
-use std::fs::OpenOptions;
-
-use slog::Drain;
-
 #[tokio::main]
 async fn main() {
-    let log_path = "rpcclient.log";
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(log_path)
-        .unwrap();
-
-    // create logger
-    let decorator = slog_term::PlainSyncDecorator::new(file);
-    let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    let logger = slog::Logger::root(drain, slog::o!());
-
-    // slog_stdlog uses the logger from slog_scope, so set a logger there
-    let _guard = slog_scope::set_global_logger(logger);
-
-    // register slog_stdlog as the log handler with the log crate
-    slog_stdlog::init().unwrap();
-
     // Get dcrd app directory, if none is found use current path.
     let mut app_dir = match app_data::get_app_data_dir("dcrd".into(), false) {
         Some(dir) => dir,
